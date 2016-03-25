d5f1a640ebcebeb1ffbb1ff4fcb4e7044f274b7a
pashaac <pasha2608199472742>
2016-03-26 00:22:20 +0300

    IPV6 static getter fixed

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -2,6 +2,7 @@ package common;
 
 import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
 
+import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -20,21 +21,16 @@ public class Constants {
     public static final String ANYCAST_ADDRESS = "FF02::1";
     public static final int SERVER_MAX_CLIENT_COUNT = 4;
 
-    public static String getIPV6() throws SocketException {
+    public static Inet6Address getIPV6() throws SocketException {
         for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
             NetworkInterface networkInterface = en.nextElement();
             if (networkInterface.getDisplayName().equals("wlan") || networkInterface.getDisplayName().equals("wlp3s0")) {
                 for (Enumeration<InetAddress> inetAddressEnumeration = networkInterface.getInetAddresses(); inetAddressEnumeration.hasMoreElements();) {
                     InetAddress inetAddress = inetAddressEnumeration.nextElement();
                     if (inetAddress.getHostAddress().contains(":")) {
-                        final String ipv6BadAddress = inetAddress.getHostAddress();
-                        final String ipv6Address = ipv6BadAddress.contains("%")
-                                ? ipv6BadAddress.substring(0, ipv6BadAddress.indexOf("%"))
-                                : ipv6BadAddress;
-                        LOG.info(networkInterface.getDisplayName() + " - " + ipv6Address);
-                        return ipv6Address;
+                        LOG.info(networkInterface.getDisplayName() + " - " + inetAddress.getHostAddress());
+                        return (Inet6Address) inetAddress;
                     }
-
                 }
             }
         }
